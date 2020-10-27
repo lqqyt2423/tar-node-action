1963f8f95c1db3404ed475bb5fb9487460513cbd
lqqyt2423 <974923609@qq.com>
2020-10-27 22:46:28 +0800

    finish

--- main.sh
+++ main.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # set -xe
+set -e
 
 echo "tar-node-action start"
+ssh_host=$1
+ssh_host_project_path=$2
+
+if [ -z "$ssh_host" ]; then
+  echo "not set ssh_host, exit"
+  exit 0
+fi
+
+if [ -z "$ssh_host_project_path" ]; then
+  echo "not set ssh_host_project_path, exit"
+  exit 0
+fi
+
+echo "ssh_host: ${ssh_host}"
 
 code_dir=$(pwd)
 echo "code_dir: $code_dir"
@@ -27,3 +42,29 @@ ls -lh ${target_tar_file}
 
 rm -rf $target_dir
 echo "removed tmp dir ${target_dir_full}"
+
+
+# deploy to ssh_host
+# exec command at remote machine
+
+# mkdir -p
+echo "remote: mkdir -p ${ssh_host_project_path}/releases"
+ssh ${ssh_host} mkdir -p ${ssh_host_project_path}/releases
+# scp
+echo "try scp ${target_tar_file}"
+scp ${target_tar_file} ${ssh_host}:${ssh_host_project_path}/releases
+# untar
+echo "remote: try untar"
+ssh ${ssh_host} tar -zx -f ${ssh_host_project_path}/releases/${target_tar_file} -C ${ssh_host_project_path}/releases
+# change soft link
+echo "remote: change soft link"
+ssh ${ssh_host} ln -sfT ${ssh_host_project_path}/releases/${target_dir} ${ssh_host_project_path}/current
+# delete tar
+echo "remote: delete ${target_tar_file}"
+ssh ${ssh_host} rm ${ssh_host_project_path}/releases/${target_tar_file}
+# todo: delete old release
+
+
+# delete local files
+echo "local: delete ${target_tar_file}"
+rm ${target_tar_file}
