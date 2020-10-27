6c05c0c6d5c1ddc4dc4f408a7d025535c0b183b0
liqiang <gymbo@gymbodeMacBook-Pro.local>
2020-10-27 19:14:44 +0800

    tar

--- main.sh
+++ main.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+# set -xe
+
 echo "tar-node-action start"
-pwd
-ls -l
-echo $PATH
-env
+
+code_dir=$(pwd)
+echo "code_dir: $code_dir"
+
+cd ..
+
+target_dir=$(date '+%Y%m%d%H%M%S')
+echo "target_dir: $target_dir"
+
+target_dir_full="$(pwd)/${target_dir}"
+echo "target_dir_full: $target_dir_full"
+
+cp -Rf $code_dir $target_dir
+echo "copied"
+
+target_tar_file="${target_dir}.tar.gz"
+echo "target_tar_file: $target_tar_file"
+
+tar -zc -f $target_tar_file $target_dir
+echo "tared to $(pwd)/${target_tar_file}"
+ls -lh ${target_tar_file}
+
+rm -rf $target_dir
+echo "removed tmp dir ${target_dir_full}"
